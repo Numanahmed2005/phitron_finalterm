86d53d2e7d37e3c7ae5ae35c07c9c589c8fbc0c5
Numanahmed2005 <numan.ahmedx@proton.me>
2025-03-23 13:33:45 +0600

    admin and customer class are done now

--- index.py
+++ index.py
@@ -1,13 +1,17 @@
-
+from abc import ABC
 
 #customer base class
-class Customer:
-    def __init__(self):
+class Customer(ABC):
+    def __init__(self,admin):
         self.balance = 0
         self.cart  = []
+        self.admin = admin
+        #self.ahmed=Admin()
     def add_balance(self):
         money = int(input())
         self.balance += money
+
+
 
     def view_money (self):
         return self.balance
@@ -15,7 +19,82 @@ class Customer:
     def view_past_order(self):
         return self.cart
 
-    def create_order(self):
+    def view_menus(self):
+        self.admin.view_menu()
 
-        
+
+#tesing phase
+
+
+
+
+#admin class
+
+class Admin:
+    def __init__(self):
+        self.cart = []
+        self.customer_account = []
+
+
+    def add_menu(self):
+        menu =input()
+
+        self.cart.append(menu)
+
+    def view_menu(self):
+        return self.cart
+
+    def remove_menu(self):
+        menu = input()
+        self.cart.remove(menu)
+
+    def add_customer_account(self):
+        #input example -> name email
+        create_account= input()
+        self.customer_account.append(create_account)
+
+
+    def remove_customer_account(self):
+        #input example -> name email
+        remove_name_email = input()
+        self.customer_account.remove(remove_name_email)
+
+    def view_customer(self):
+        return self.customer_account
+
+    def modify_menu(self):
+        #input example -> menu price
+        value=input()
+        self.cart.insert(0,value)
+
+#test phase of admin class ->
+#cusomers = Admin()
+#cusomers.add_customer_account()
+#print(cusomers.view_customer())
+
+#cusomers.add_menu()
+#cusomers.add_menu()
+#print(cusomers.view_menu())
+
+my_admin  = Admin()
+
+Customers = Customer(my_admin)
+my_admin.add_menu()
+print(Customers.view_menus())
+
+
+# Resturent class
+
+class Resturant:
+
+    def manage_menu(self):
+        pass
+
+
+
+
+
+
+
+
 
