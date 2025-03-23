c0d66009e56c51aede46c09f4fcce04e333b571b
Numanahmed2005 <numan.ahmedx@proton.me>
2025-03-23 13:33:45 +0600

    modify the code and facing hiarchy problem in class

--- index.py
+++ index.py
@@ -1,21 +1,103 @@
 
 
 #customer base class
-class Customer:
+
+
+#tesing phase
+
+
+
+
+#admin class
+
+class Admin:
+    def __init__(self):
+        self.cart = []
+        self.customer_account = []
+
+
+
+    def add_menu(self):
+        menu =input()
+
+        self.cart.append(menu)
+
+    def view_menu(self):
+        return self.cart
+
+    def remove_menu(self):
+        menu = input()
+        self.cart.remove(menu)
+
+    def add_customer_account(self):
+        #input example -> name email
+        create_account= input()
+        self.customer_account.append(create_account)
+
+
+    def remove_customer_account(self):
+        #input example -> name email
+        remove_name_email = input()
+        self.customer_account.remove(remove_name_email)
+
+    def view_customer(self):
+        return self.customer_account
+
+    def modify_menu(self):
+        #input example -> menu price
+        value=input()
+        self.cart.insert(0,value)
+
+#test phase of admin class ->
+#cusomers = Admin()
+#cusomers.add_customer_account()
+#print(cusomers.view_customer())
+
+#cusomers.add_menu()
+#cusomers.add_menu()
+#print(cusomers.view_menu())
+
+
+#customer class
+class Customer(Admin):
     def __init__(self):
         self.balance = 0
-        self.cart  = []
+        self.order_cart = []
+
+
+        #self.ahmed=Admin()
     def add_balance(self):
         money = int(input())
         self.balance += money
+
+
 
     def view_money (self):
         return self.balance
 
     def view_past_order(self):
-        return self.cart
+        return self.order_cart
 
-    def create_order(self):
+    def view_menus(self):
+        pass
 
-        
+
+
+
+
+
+# Resturent class
+
+class Resturant:
+
+    def manage_menu(self):
+        pass
+
+
+
+
+
+
+
+
 
